0c89ca32c754da01058755252ac5b9a383e81481
Getonto3 <getontomoses18@gmail.com>
2025-01-22 21:06:59 +0300

    replacing guest names

---
+++ updated_list.py
@@ -0,0 +1,8 @@
+guestlist = ["Angeline", "Virginia", "Keru"]
+print(f"unfortunately {guestlist[1]} couldnt makeit ")
+
+guestlist[1] = 'Nana'
+
+for guest in guestlist:
+    print(f"{guest} welcome to dinner")
+
