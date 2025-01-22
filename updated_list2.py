b766827f9efcad0e2545744152b95544b773bf89
Getonto3 <getontomoses18@gmail.com>
2025-01-22 21:16:37 +0300

    more guests  at the table

---
+++ updated_list2.py
@@ -0,0 +1,8 @@
+guestlist = ["Angeline", "Virginia", "Keru"]
+
+guestlist.insert(0,"Mufasa")
+guestlist.insert(3, "Aaron")
+guestlist.append( "Pieere")
+
+for guest in guestlist:
+    print(f"Hey {guest} welcome to dinner")
