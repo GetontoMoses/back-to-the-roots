58bd0745c39f9e95b32469b995ae4e2a2ce6a392
Getonto3 <getontomoses18@gmail.com>
2025-01-23 14:25:16 +0300

    manipulating lists

--- working_with_range.py
+++ working_with_range.py
@@ -1,2 +1,20 @@
 squares = [value**2 for value in range(1,10)]
 print(squares)
+
+# first 20 numbers
+numbers = [value for value in range(1,21)]
+print(numbers)
+
+# one million
+numbers = [value for value in range(1,1000001)]
+print(min(numbers))
+print(max(numbers))
+print(sum(numbers))
+
+# Odd numbers
+numbers = [value for value in range(1,21,2)]
+print(numbers)
+
+#multiples of three
+numbers = [value*3 for value in range(3,31)]
+print(numbers)
