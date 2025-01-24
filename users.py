0ae07bca2f56b6cfffb17ec7b585994be282d6e5
Getonto3 <getontomoses18@gmail.com>
2025-01-24 18:05:02 +0300

    twitter

---
+++ users.py
@@ -0,0 +1,9 @@
+kox = ["Admin", "Mufasa", "Elon", "Musk", "Barrack", "Lazor", "GEazy", "Rexa"]
+
+for user in kox:
+    if user.lower == "admin":
+        print(" Hello superuser, welcome on board,status page?")
+    else:
+        print(f"Hello {user}, welcome to day 1 of 121")
+        
+print(kox[2])
