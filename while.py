c3d2341045d57c42883b4787dcd6f2d144b24b3a
Getonto3 <getontomoses18@gmail.com>
2025-01-26 23:56:32 +0300

    I can now function

--- while.py
+++ while.py
@@ -1,5 +1,10 @@
 timer = 1
 
 while timer <= 5:
-    print(f"Wake up!!! This is snooze number {timer}")
+    #print(f"Wake up!!! This is snooze number {timer}")
     timer+=1
+    
+def printing(size,message):
+    print(f"Your shirt is size {size} and you would like '{message}' printed on it.")
+    
+printing(size=30,message="Hello world")
