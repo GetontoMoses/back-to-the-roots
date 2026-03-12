f0e86da264af6d96fe8826e254c8f39cd6419d0e
Getonto3 <getontomoses18@gmail.com>
2026-03-12 23:25:37 +0300

    loops

---
+++ lupin.py
@@ -0,0 +1,12 @@
+cars = [
+    "Bently",
+    "Range Rover",
+    "Mercedes",
+    "Toyota",
+    "Bently",
+    "Bently",
+    "Subaru",
+]
+print("In a little while, we getting: ")
+for car in cars:
+    print(car)
